95b0998c2b6ce1db7eac32210d9197089835433c
ghxusr <nldevsol@protonmail.com>
2024-05-30 16:17:48 +0200

    Solution for 07_tempConversion in javascript-exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //Round to one decimal
+  let tempCelcius = Math.round(((temp - 32) * (5 / 9)) * 10) / 10;
+  return tempCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempFahrenheit = Math.round(((temp * (9 / 5)) + 32) * 10) / 10;
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
