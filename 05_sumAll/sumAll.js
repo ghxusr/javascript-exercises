9649c6455836b741cd1e2d80dee8c0acc1088c57
ghxusr <nldevsol@protonmail.com>
2024-05-30 15:00:18 +0200

    Solution for 05_sumAll in javascript-exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let numberSum = 0;
+    let smallNumber = 0;
+    let bigNumber = 0;
 
+    if (number1 > number2) {
+        bigNumber = number1;
+        smallNumber = number2;
+    } 
+    else if (number1 < number2) {
+        bigNumber = number2; 
+        smallNumber = number1;
+    }
+        
+    if (smallNumber >= 0 && bigNumber >= 0) {
+        for (let i = smallNumber; i <= bigNumber; i++) {
+            numberSum = numberSum + i;
+        }
+    }
+
+    if ((number1 < 0 || number2 < 0) || 
+    (Number.isInteger(number1) === false || Number.isInteger(number2) === false)) {
+        return "ERROR"
+    } 
+    else return numberSum;
+    
 };
 
 // Do not edit below this line
