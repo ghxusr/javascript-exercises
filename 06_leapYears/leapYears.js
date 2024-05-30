779cef616ca8c96dad3fa5e4853ecac2fb00194d
ghxusr <nldevsol@protonmail.com>
2024-05-30 15:59:28 +0200

    Solution for 06_leapYears in javascript-exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let answer = false;
 
+    if (year % 4 === 0 && year % 100 != 0) {
+        answer = true;
+    }
+
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        answer = true;
+    }
+    return answer;
 };
 
 // Do not edit below this line
