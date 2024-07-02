b4d0181e852a592d1664658bd8eb5c6651f4d36a
ghxusr <nldevsol@protonmail.com>
2024-07-02 23:12:34 +0200

    Solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validChar = "0123456789abcdefghijklmnopqrstuvwxyz";
+    const inputString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => validChar.includes(char))
+        .join("");
+                    
+    const reverseString = Array.from(inputString)
+        .reverse()
+        .join("");
 
+    return inputString === reverseString
 };
 
 // Do not edit below this line
