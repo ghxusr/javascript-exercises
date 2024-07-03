632b2f853bc9d9bd3f13032906a3e6c10d4293e4
ghxusr <nldevsol@protonmail.com>
2024-07-03 17:26:24 +0200

    Solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence = [0,1];
+    let index = 0;
+    let validNum = parseInt(num);
 
+    if (validNum === 0) return 0;
+    if (validNum < 0) return "OOPS";
+
+    while (index < validNum) {
+        sequence.push(sequence.slice(-2).reduce((total, current) => total + current, 0));
+        index++;
+    }
+
+    return sequence[index];
 };
 
 // Do not edit below this line
