aaf9a7eb21402ac73d1aa7268b7d8e148f221e50
ghxusr <nldevsol@protonmail.com>
2024-07-04 16:24:51 +0200

    Solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestAge = people.sort((a, b) => {
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return -1
+        } else if ((a.yearOfDeath === undefined || b.yearOfDeath === undefined && 
+                a.yearOfBirth < b.yearOfBirth)) {
+            return -1;
+        } 
+    });
+    return oldestAge[0];
 };
 
 // Do not edit below this line
